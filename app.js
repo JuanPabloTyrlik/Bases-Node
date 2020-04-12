89076136dbaa9d4a9a09a4c693d00686da1eb9c9
Juan Pablo Tyrlik <juan.pablo.tyrlik@gmail.com>
2020-04-12 17:55:05 -0300

    Se toman los parametros usando Yargs

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 // Imports
-
+const argv = require('yargs')
+    .command('listar', 'Imprime la tabla de multiplicar en consola', {
+        base: {
+            alias: 'b',
+            demand: true
+        },
+        limite: {
+            alias: 'l',
+            default: 10
+        }
+    })
+    .help()
+    .argv;
 const { crearArchivo } = require('./multiplicar/multiplicar');
 
-let base = process.argv[2].split('=')[1];
+// let base = process.argv[2].split('=')[1];
 
-crearArchivo(base)
-    .then(archivo => console.log(`El archivo ${archivo} ha sido creado.`))
-    .catch(err => console.log(err));
+console.log(argv.base);
+console.log(argv.limite);
+
+
+// crearArchivo(argv.base)
+//     .then(archivo => console.log(`El archivo ${archivo} ha sido creado.`))
+//     .catch(err => console.log(err));
